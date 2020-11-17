6c778fcfe4085d72708997542f581f2d263df426
Ricardo Borillo <borillo@gmail.com>
2020-11-17 18:41:54 +0100

    Customon matcher for fixing decimals

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -2,4 +2,31 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import expect from "expect";
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toEqualRounded(value: number): R;
+    }
+  }
+}
+
+expect.extend({
+  toEqualRounded(received: number, computed: number) {
+    if (Number(received.toFixed(2)) === computed) {
+      return {
+        pass: true,
+      };
+    }
+
+    return {
+      message: () =>
+        `expected ${received} to equal ${computed} with two decimal precision`,
+      pass: false,
+    };
+  },
+});
